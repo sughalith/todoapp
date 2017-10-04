695950c206fb865d050fe456f2ee116441289225
Arkadiusz Stankiewicz <arkadiuszstankiewicz@MacBook-Pro-test.local>
2017-10-04 13:17:46 +0200

    Complete app

--- To Do App/FirstViewController.swift
+++ To Do App/FirstViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var items: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        if let tempItems = itemsObject as? [String]{
+            items = tempItems
+            items = items.reversed()
+        }
+        tableView.reloadData()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            items.remove(at: indexPath.row)
+            tableView.reloadData()
+            
+            UserDefaults.standard.set(items, forKey: "items")
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
 }
 
--- To Do App/SecondViewController.swift
+++ To Do App/SecondViewController.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var itemTextField: UITextField!
+    @IBAction func add(_ sender: Any) {
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        var items: [String]
+        
+        if let tempItems = itemsObject as? [String]{
+            items = tempItems
+            items.append(itemTextField.text!)
+        }
+        else{
+            items = [itemTextField.text!]
+        }
+        
+        UserDefaults.standard.set(items, forKey: "items")
+        itemTextField.text = ""
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +38,14 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
